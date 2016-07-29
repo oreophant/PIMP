e1fe12224f3b7f33c7be687f7bc5846d8b4a45ee
Oreophant <ruida@dingansi.ch>
2016-07-30 04:22:20 +0800

    Adds random Quote to login, adds graphs

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -15,7 +15,24 @@ angular.module('pimp')
       $location.path('/dashboard');
 
       return false;
-    }
+    };
+    $scope.quote= function(){
+
+      var quotes= [
+        '“A penny here, and a dollar there, placed at interest, goes on accumulating, and in this way the desired result is attained. It requires some training, perhaps, to accomplish this economy, but when once used to it, you will find there is more satisfaction in rational saving than in irrational spending.” -P. T. Barnum',
+        '“Many people take no care of their money till they come nearly to the end of it, and others do just the same with their time.” -Johann Wolfgang von Goethe',
+        '“Old men are always advising young men to save money. That is bad advice. Don’t save every nickel. Invest in yourself. I never saved a dollar until I was forty years old.” -Henry Ford',
+        '“My old father used to have a saying:  If you make a bad bargain, hug it all the tighter. ” -Abraham Lincoln',
+        '“Money never made a man happy yet, nor will it. The more a man has, the more he wants. Instead of filling a vacuum, it makes one.” -Ben Franklin',
+        '“Too many people spend money they haven’t earned, to buy things they don’t want, to impress people they don’t like.” -Will Smith',
+        '“Only buy something that you’d be perfectly happy to hold if the market shut down for ten years.” -Warren Buffett'
+      ];
+
+      var numQuote = Math.floor(Math.random()*(6));
+
+      return quotes[numQuote];
+
+    };
 
 
   });
--- app/scripts/graph.js
+++ app/scripts/graph.js
@@ -1,74 +1,77 @@
-google.charts.load('current', {'packages':['corechart', 'treemap']});
-
-google.charts.setOnLoadCallback(drawFirstChart);
-
-google.charts.setOnLoadCallback(drawSecondChart);
-
-function drawFirstChart() {
-
-  var data = google.visualization.arrayToDataTable([
-    ['Task', 'Hours per Day'],
-    ['Work',     11],
-    ['Eat',      2],
-    ['Commute',  2],
-    ['Watch TV', 2],
-    ['Sleep',    7]
-  ]);
-
-  var options = {
-    title: 'My Daily Activities'
-  };
-
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-  chart.draw(data, options);
-};
+google.charts.load('current', {'packages':['corechart', 'treemap', 'gauge']});
 
 
+google.charts.setOnLoadCallback(drawLifeChart);
+google.charts.setOnLoadCallback(drawGaugeChart);
 
 
-      function drawSecondChart() {
+      function drawLifeChart() {
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'ID');
+      data.addColumn('string', 'Parent');
+      data.addColumn('number', 'Number Of Lines');
+      data.addRows([
+        ['Life Planner', null, 0],
+
+        ['House Ownership', 'Life Planner', null],
+        ['Marriage', 'Life Planner', null],
+        ['Retirement', 'Life Planner', null],
+
+        ['House price', 'House Ownership', 500000/3],
+        ['Stampduty', 'House Ownership', 3000],
+        ['lawyer fee', 'House Ownership', 5000],
+        ['Money Money', 'House Ownership', 30000],
+        ['Basic needs', 'Retirement', 50000],
+        ['History Of King John', 'Retirement', 60000],
+        ['Antony And Cleopatra', 'Marriage', 80000],
+        ['Coriolanus', 'Marriage', 12313],
+        ['Cymbeline', 'Marriage', 12312],
+        ['The Tragedy of Hamlet, Prince of Denmark', 'Marriage', 12312],
+        ['Julius Caesar', 'Marriage', 12312],
+        ['Comedy Of Errors', 'Retirement', 11231],
+        ['Merchant Of Venice', 'Retirement', 12312],
+      ]);
+      var tree = new google.visualization.TreeMap(document.getElementById('chart_div'));
+
+      var options = {
+        highlightOnMouseOver: true,
+        maxDepth: 1,
+        maxPostDepth: 2,
+        /*
+        minHighlightColor: '#8c6bb1',
+        midHighlightColor: '#9ebcda',
+        maxHighlightColor: '#edf8fb',
+        minColor: '#145ECC',
+        midColor: '#B22411',
+        maxColor: '#C8FF19',
+        */
+        headerHeight: 30,
+        showScale: false,
+        height: 500,
+        useWeightedAverageForAggregation: true
+      };
+
+        tree.draw(data, options);
+
+      };
+
+
+      function drawGaugeChart() {
+
         var data = google.visualization.arrayToDataTable([
-          ['Location', 'Parent', 'Market trade volume (size)', 'Market increase/decrease (color)'],
-          ['Global',    null,                 0,                               0],
-          ['America',   'Global',             0,                               0],
-          ['Europe',    'Global',             0,                               0],
-          ['Asia',      'Global',             0,                               0],
-          ['Australia', 'Global',             0,                               0],
-          ['Africa',    'Global',             0,                               0],
-          ['Brazil',    'America',            11,                              10],
-          ['USA',       'America',            52,                              31],
-          ['Mexico',    'America',            24,                              12],
-          ['Canada',    'America',            16,                              -23],
-          ['France',    'Europe',             42,                              -11],
-          ['Germany',   'Europe',             31,                              -2],
-          ['Sweden',    'Europe',             22,                              -13],
-          ['Italy',     'Europe',             17,                              4],
-          ['UK',        'Europe',             21,                              -5],
-          ['China',     'Asia',               36,                              4],
-          ['Japan',     'Asia',               20,                              -12],
-          ['India',     'Asia',               40,                              63],
-          ['Laos',      'Asia',               4,                               34],
-          ['Mongolia',  'Asia',               1,                               -5],
-          ['Israel',    'Asia',               12,                              24],
-          ['Iran',      'Asia',               18,                              13],
-          ['Pakistan',  'Asia',               11,                              -52],
-          ['Egypt',     'Africa',             21,                              0],
-          ['S. Africa', 'Africa',             30,                              43],
-          ['Sudan',     'Africa',             12,                              2],
-          ['Congo',     'Africa',             10,                              12],
-          ['Zaire',     'Africa',             8,                               10]
+          ['Label', 'Value'],
+          ['Goal %', 80],
         ]);
 
-        tree = new google.visualization.TreeMap(document.getElementById('chart_div'));
+        var options = {
+          width: 400, height: 120,
+          redFrom: 90, redTo: 100,
+          yellowFrom:75, yellowTo: 90,
+          minorTicks: 5
+        };
 
-        tree.draw(data, {
-          minColor: '#f00',
-          midColor: '#ddd',
-          maxColor: '#0d0',
-          headerHeight: 15,
-          fontColor: 'black',
-          showScale: true
-        });
+        var chart = new google.visualization.Gauge(document.getElementById('chart_div2'));
 
-      }
+        chart.draw(data, options);
+
+      };
